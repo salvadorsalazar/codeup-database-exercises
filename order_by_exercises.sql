bac13ca20b86fc6e9314820e83cf40cc0353a29f
salvador salazar <salvador.g.salazar79@gmail.com>
2021-10-07 23:55:35 -0500

    added order by exercises

---
+++ order_by_exercises.sql
@@ -0,0 +1,20 @@
+use employees;
+
+select * from employees;
+
+
+# Modify your first query to order by first name. The first result should be Irena Reutenauer and the last result should be Vidya Demeyer.
+
+
+
+# Update the query to order by first name and then last gitname. The first result should now be Irena Acton and the last should be Vidya Boguraev.
+
+
+# Change the ORDER BY clause so that you order by last name before first name. Your first result should still be Irena Acton but now the last result should be Vidya Quittner.
+
+
+# Update your queries for employees with 'E' in their last name to sort the results by their employee number. Your results should not change!
+
+
+
+# Now reverse the sort order for both queries and compare results.
